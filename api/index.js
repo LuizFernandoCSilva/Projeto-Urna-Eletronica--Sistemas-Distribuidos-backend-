0d4005ff44b93107cae7a9ec83b5859457379171
Luiz Fernando <d2022003915@unifei.edu.br>
2024-10-01 17:40:34 -0300

    Deploy Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+// api/index.js
 import serverless from 'serverless-http';
-import app from '../app.js'; 
-export const handler = serverless(app);
+import app from '../app.js'; // Certifique-se de que o caminho está correto
+
+const handler = serverless(app);
+
+export default handler;
